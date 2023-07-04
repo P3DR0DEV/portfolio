e45ea3f3472668334bbc36e3187c1bbaa74f74ed
Pedro Henrique Campos Mendes <pedrohenriquecamposmendes@hotmail.com>
2023-07-04 16:04:04 -0300

    feat: update css

--- src/Components/Skills/Skills.tsx
+++ src/Components/Skills/Skills.tsx
@@ -2,7 +2,7 @@ import { IData } from "@/types"
 
 export function Skills({ skill }: { skill: IData }) {
   return (
-    <div className="bg-[#e2e8f0] h-96 rounded-md text-[#4a5568] duration-500">
+    <div className="bg-[#e2e8f0] h-96 rounded-md text-[#4a5568] min-w-full">
       <h3 className="text-center text-2xl capitalize  font-bold py-2">
         {skill.title}
       </h3>
@@ -11,9 +11,9 @@ export function Skills({ skill }: { skill: IData }) {
           return (
             <div
               key={skill.name}
-              className="flex justify-between border-b border-[#918e9b86] h-9 items-center"
+              className="flex justify-between text-lg border-b border-[#918e9b86] py-3 px-2 items-center"
             >
-              <p className="font-bold capitalize text-base">{skill.name}</p>
+              <p className="font-bold capitalize">{skill.name}</p>
               <p className="">Skill Level: {skill.skill}/4</p>
             </div>
           )
