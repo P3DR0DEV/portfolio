1124995cfc03d9a45f9475e0ac5dcdffc3332978
Pedro Henrique Campos Mendes <pedrohenriquecamposmendes@hotmail.com>
2023-07-05 09:44:27 -0300

    fix: fixed scroll snap at the skills field

--- src/Components/Skills/Skills.tsx
+++ src/Components/Skills/Skills.tsx
@@ -2,7 +2,7 @@ import { IData } from "@/types"
 
 export function Skills({ skill }: { skill: IData }) {
   return (
-    <div className="bg-[#e2e8f0] h-96 rounded-md text-[#4a5568] min-w-full  overflow-auto">
+    <div className="bg-[#e2e8f0] h-96 rounded-md text-[#4a5568] min-w-full  overflow-auto snap-center">
       <h3 className="text-center text-2xl capitalize  font-bold py-2">
         {skill.title}
       </h3>
@@ -14,7 +14,6 @@ export function Skills({ skill }: { skill: IData }) {
               className="flex justify-between text-lg border-b border-[#918e9b86] py-3 px-2 items-center"
             >
               <p className="font-bold capitalize">{skill.name}</p>
-              <p className="">Skill Level: {skill.skill}/4</p>
             </div>
           )
         })}
