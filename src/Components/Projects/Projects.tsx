b60e159ae8d645e7711b39cd279acb0a8a5cdc35
Pedro Henrique Campos Mendes <pedrohenriquecamposmendes@hotmail.com>
2023-07-06 12:43:59 -0300

    feat: update render ProjectCard

--- src/Components/Projects/Projects.tsx
+++ src/Components/Projects/Projects.tsx
@@ -1,10 +1,21 @@
 import { ProjectCard } from "./ProjectCard"
+import { projectsData } from "@/util"
 
 export function Projects() {
   return (
-    <section className="h-screen p-5">
-      <h2 className="text-lg">My Projects</h2>
-      <ProjectCard isOdd={false} />
+    <section className="p-5">
+      <h2 className="text-lg mb-2">My Projects</h2>
+      <div className="flex flex-col gap-4">
+        {projectsData.map((project) => (
+          <ProjectCard
+            key={project.id}
+            ProjectName={project.ProjectName}
+            description={project.description}
+            images={project.images}
+            isOdd={(project.id as number) % 2 === 0}
+          />
+        ))}
+      </div>
     </section>
   )
 }
