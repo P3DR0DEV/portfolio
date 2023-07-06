fc53acf910ac104eeb9912d5512dcaf97f8403a5
Pedro Henrique Campos Mendes <pedrohenriquecamposmendes@hotmail.com>
2023-07-06 12:44:48 -0300

    feat: Update Style and Render component Project Card. TODO: carousel Images

--- src/Components/Projects/ProjectCard.tsx
+++ src/Components/Projects/ProjectCard.tsx
@@ -1,10 +1,29 @@
-export function ProjectCard({ isOdd }: { isOdd: boolean }) {
-  const align = isOdd ? "items-start" : "items-end"
+import { ProjectBody } from "@/types"
+interface ProjectBodyProps extends ProjectBody {
+  isOdd: boolean
+}
+
+export function ProjectCard({
+  ProjectName,
+  description,
+  images,
+  isOdd,
+}: ProjectBodyProps) {
+  const align = isOdd ? "flex-row" : "flex-row-reverse"
+
+  //TODO Make the images component
   return (
-    <div className={`bg-[#262626] px-3 py-1 flex flex-col ${align}`}>
-      <div>images</div>
-      <div>
-        <p></p>
+    <div className={`bg-[#262626] px-3 py-1 rounded-md`}>
+      <h1 className="text-2xl font-bold">{ProjectName}</h1>
+      <div
+        className={`flex flex-col  md:flex md:${align} md:justify-between md:gap-3`}
+      >
+        <div className="md:self-start self-center">images</div>
+        <div className="md:max-w-[40%] text-justify flex flex-col gap-3 mt-3">
+          {description.map((text) => (
+            <p key={text}>{text}</p>
+          ))}
+        </div>
       </div>
     </div>
   )
