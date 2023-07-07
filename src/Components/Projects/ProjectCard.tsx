4f3dcc50961548a6ef633dce9e3f329d207c045e
Pedro Henrique <pedrohenriquecamposmendes@hotmail.com>
2023-07-06 23:50:21 -0300

    feat: ternary until i find a better solution

--- src/Components/Projects/ProjectCard.tsx
+++ src/Components/Projects/ProjectCard.tsx
@@ -9,22 +9,34 @@ export function ProjectCard({
   images,
   isOdd,
 }: ProjectBodyProps) {
-  const align = isOdd ? "flex-row" : "flex-row-reverse"
-
   //TODO Make the images component
+
   return (
     <div className={`bg-[#262626] px-3 py-3 rounded-md`}>
       <h1 className="text-2xl font-bold md:text-center">{ProjectName}</h1>
-      <div
-        className={`flex flex-col  md:flex md:${align} md:justify-between md:gap-3`}
-      >
-        <div className="md:self-start self-center">images</div>
-        <div className="md:max-w-[40%] text-justify flex flex-col gap-3 mt-3">
-          {description.map((text) => (
-            <p key={text}>{text}</p>
-          ))}
+      {isOdd ? (
+        <div
+          className={`flex flex-col  md:flex md:flex-row md:justify-between md:gap-3`}
+        >
+          <div className="md:self-start self-center">images</div>
+          <div className="md:max-w-[40%] text-justify flex flex-col gap-3 mt-3">
+            {description.map((text) => (
+              <p key={text}>{text}</p>
+            ))}
+          </div>
         </div>
-      </div>
+      ) : (
+        <div
+          className={`flex flex-col  md:flex md:flex-row-reverse md:justify-between md:gap-3`}
+        >
+          <div className="md:self-start self-center">images</div>
+          <div className="md:max-w-[40%] text-justify flex flex-col gap-3 mt-3">
+            {description.map((text) => (
+              <p key={text}>{text}</p>
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   )
 }
