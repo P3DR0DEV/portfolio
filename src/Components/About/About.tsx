94eb90585fd038d36dee5b44d048b200a200d861
Pedro Mendes <pedrohenriquecamposmendes@hotmail.com>
2023-10-02 17:08:59 -0300

    feat: update footer style

--- src/Components/About/About.tsx
+++ src/Components/About/About.tsx
@@ -4,16 +4,23 @@ export function About() {
       <h3 className="text-center text-2xl font-normal">
         A LITTLE BIT ABOUT ME
       </h3>
-      <p className="mx-auto my-8 w-2/3 text-center">
-        My name is Pedro Mendes and I'm a Full-Stack Developer passionate about
-        tecnology and thirsty of knowledge. <br /> I was 14 years old when i
-        made my first website, using only HTML and CSS, and I started to delve
-        into programming in the middle of the COVID pandamic, using python.
-        After learning the concepts of programming logic, I started to study the
-        web-dev trinity (HTML, CSS and JavaScript). I can't see myself without
-        programming, it's my job and my hobby. Nowadays i build my web apps with
-        React TSX and Node.
-      </p>
+      <div className="mx-auto my-8 md:w-2/3 md:text-center text-justify">
+        <p>
+          My name is Pedro Mendes and I'm a Full-Stack Developer passionate
+          about tecnology and thirsty of knowledge.
+        </p>
+        <p>
+          I was 14 years old when i made my first website, using only HTML and
+          CSS, and I started to delve into programming in the middle of the
+          COVID pandamic, using python.
+        </p>
+        <p>
+          After learning the concepts of programming logic, I started to study
+          the web-dev trinity (HTML, CSS and JavaScript). I can't see myself
+          without programming, it's my job and my hobby.
+        </p>
+        <p>Nowadays i build my web apps with React TSX and Node.</p>
+      </div>
     </section>
   )
 }
